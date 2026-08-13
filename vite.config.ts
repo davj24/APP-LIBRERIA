f0c28994135d14d29cd0e371f6af3200185914da
davj24 <302196168+davj24@users.noreply.github.com>
2026-08-13 13:02:58 +0200

    Update base path in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { VitePWA } from 'vite-plugin-pwa'
-import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',
   server: {
     host: true,
     port: 5173
