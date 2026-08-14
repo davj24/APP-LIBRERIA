e1b094bef2ce499c9bcff611d34bcb855a1875d6
davj24 <302196168+davj24@users.noreply.github.com>
2026-08-14 22:15:40 +0200

    Auto unregister stale SW

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,18 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// Unregister stale service workers to ensure immediate update of live changes
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    for (const registration of registrations) {
+      registration.unregister();
+    }
+  });
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
